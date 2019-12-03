2bab43575fbe0fb3ad00970569968417f3dabc38
Jam <superwangjianmin@163.com>
2019-12-03 11:05:23 +0800

    Initial commit

---
+++ Example/YDCategorySDK/YDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YDAppDelegate.h
+//  YDCategorySDK
+//
+//  Created by JamWangJam on 12/03/2019.
+//  Copyright (c) 2019 JamWangJam. All rights reserved.
+//
+
+@import UIKit;
+
+@interface YDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/YDCategorySDK/YDViewController.h
@@ -0,0 +1,13 @@
+//
+//  YDViewController.h
+//  YDCategorySDK
+//
+//  Created by JamWangJam on 12/03/2019.
+//  Copyright (c) 2019 JamWangJam. All rights reserved.
+//
+
+@import UIKit;
+
+@interface YDViewController : UIViewController
+
+@end
